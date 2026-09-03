d818cde9859962ff460076ef41e959dfc1ca11e4
Connor.H <connor.h20@hotmail.com>
2026-09-03 16:55:24 +1000

    minor change

--- Assets/Scripts/Game Behaviour/Modules/Game State Behaviours/PyramidGameStateBehaviour.cs
+++ Assets/Scripts/Game Behaviour/Modules/Game State Behaviours/PyramidGameStateBehaviour.cs
@@ -1,6 +1,7 @@
 namespace CardGameArchive.Behaviours
 {
 	using System.Linq;
+	using System.Collections.Generic;
 	using UnityEngine;
 
 	[CreateAssetMenu(fileName = "PyramidGameStateBehaviour", menuName = "Card Game Archive/Game Behaviour/Modules/Game State Behaviours/Pyramid")]
@@ -9,9 +10,10 @@ namespace CardGameArchive.Behaviours
 		public override bool IsGameStuck()
 		{
 			// Game is stuck if the only valid pair for a card is being blocked by this card
-			foreach (Card card in GameBoard.Instance.GetZoneParents(GameBoard.CardZone.Tableau).Where(o => o.BottomCard != null && o.BottomCard.Interactable).Select(o => o.BottomCard))
+			List<Card> tableauCards = GameBoard.Instance.GetZoneParents(GameBoard.CardZone.Tableau).Where(o => o.BottomCard != null && o.BottomCard.Interactable).Select(o => o.BottomCard).ToList();
+			foreach (Card card in tableauCards)
 			{
-
+				
 			}
 
 			if (StandardGameManager.Instance.Variant == GameTerms.GameVariant.PyramidTraditional)
